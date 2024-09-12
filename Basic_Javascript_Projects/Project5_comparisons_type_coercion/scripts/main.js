1ef6f6806b0bf2631f15feb698abc18894c6fcd1
Ian McAllister <iemcallister@gmail.com>
2024-09-12 11:40:01 -0700

    Adding type coercion

---
+++ Basic_Javascript_Projects/Project5_comparisons_type_coercion/scripts/main.js
@@ -0,0 +1,9 @@
+
+/* This will demonstrate the typeof function, by printing 'string' to the screen' */
+document.write(typeof 'this is a test');
+
+/* This demonstrates type coercion in that a string and a number are combined to form a string (not a number) */
+document.write(' 10' + 10);
+
+/* This demonstrates the usage of the following operators */
+document.write(" ", 11 == 12, " ", 10 === '10', " ", 3 > 2, " ", 5 < 10, " ", true && false == true, " ", true || false == true, " ", !true);

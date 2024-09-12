11eb500728ab7aa6310e5d9760951578c29efb0d
Ian McAllister <iemcallister@gmail.com>
2024-09-12 08:08:09 -0700

    Adding Basic Javascript Expressions Folder

---
+++ Basic_Javascript_Projects/Project1_expressions_alert/scripts/main.js
@@ -0,0 +1,5 @@
+var Sent1 = "This is the beginning of the string" /*Defining the first variable*/
+var Sent2 = " and this is the end of the string" /* Defining the second variable */
+var Sent3 = Sent1 + Sent2; /*We're concatenating a string here*/
+document.write(B);
+window.alert(B);
